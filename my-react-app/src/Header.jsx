e0af72c8a8371edeaa8a84bec86b6bb2ca2357f4
algoreddy <helloyash99@gmail.com>
2025-10-29 00:58:30 +0530

    got to know the react basics & basic file structure & what are the componenets etc etc

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <ul>
+        <li>
+          {" "}
+          <a href="#"></a>Home
+        </li>
+        <li>
+          <a href="#"></a>About
+        </li>
+        <li>
+          <a href="#"></a>Service
+        </li>
+        <li>
+          <a href="#"></a>Contact
+        </li>
+      </ul>
+      <hr />
+    </header>
+  );
+}
+export default Header;
