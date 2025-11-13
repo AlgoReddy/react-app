882172cb7428d8f3488db38d5d63dc78127d0424
algoreddy <helloyash99@gmail.com>
2025-11-14 00:39:26 +0530

    cards are created

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Bookmark } from "lucide-react";
+import Card from "./components/card";
+const App = () => {
+  return (
+    <div className="parent">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
