11829a026e7a04b6ea3d61ddbbfa298a8f94888d
algoreddy <helloyash99@gmail.com>
2025-11-21 01:28:31 +0530

    functions  completed and got to know how to use the functions in the react in a correct way

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+
+
+
+// import React from "react";
+
+// const App = () => {
+//   const pageScrolling = () => {
+//     if (elem > 0) {
+//       console.log("up to down scrolling");
+//     } else {
+//       console.log("down to up scrolling");
+//     }
+//   };
+//   return (
+//     <div
+//       onWheel={(elem) => {
+//         pageScrolling(elem.deltaY);
+//       }}
+//     >
+//       <div className="page1"></div>
+//       <div className="page2"></div>
+//       <div className="page3"></div>
+//     </div>
+//   );
+// };
+
+// export default App;
