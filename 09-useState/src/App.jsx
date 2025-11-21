a81d0fa222294c732c7bccd32d5e8cc8bdf86a77
algoreddy <helloyash99@gmail.com>
2025-11-21 16:43:54 +0530

    created a small counter program using the react

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -7,10 +7,21 @@ const App = () => {
         setfirst("Yeswanth Reddy Shasankota");
         setAge(23);
     }
+const [number, Setnumber] = useState(0);
+function increaseNum() {
+    Setnumber(number+1);
+}
+function decreaseNum() {
+    Setnumber(number-1);
+}
   return (
     <>
-    <h1>My name is {first}  & my age is {age} </h1>
-    <button onClick={changeName}>click</button>
+    {/* <h1>My name is {first}  & my age is {age} </h1>
+    <button onClick={changeName}>click</button> */}
+
+    <h1>{number}</h1>
+    <button onClick={decreaseNum}  onMouseEnter={decreaseNum} onKeyDown={decreaseNum}>Decrease</button>
+    <button onClick={increaseNum} onMouseEnter={increaseNum} onKeyUp={increaseNum}>Increase </button>
 
     </>
   )
